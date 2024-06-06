39144e63c0170f95d6115641fb1c4f2938ae4d2f
Matteo <matteo.courquin@fortuneo.com>
2024-06-06 10:58:42 +0200

    rocket: Fix IAM role from dynamoDb

--- src/vehicles.ts
+++ src/vehicles.ts
@@ -1,5 +1,11 @@
 import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
-import { DeleteCommand, DynamoDBDocumentClient, PutCommand, ScanCommand, UpdateCommand } from '@aws-sdk/lib-dynamodb';
+import {
+  DeleteCommand,
+  DynamoDBDocumentClient,
+  PutCommand,
+  ScanCommand,
+  UpdateCommand,
+} from '@aws-sdk/lib-dynamodb';
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 
 const client = new DynamoDBClient({});
@@ -24,7 +30,9 @@ interface VehicleUpdateRequest {
   number_of_owner?: number;
 }
 
-export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+export const handler = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
   switch (event.httpMethod) {
     case 'POST':
       return postVehicles(event);
@@ -50,11 +58,22 @@ export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayPr
 const postVehicles = async (event: any) => {
   try {
     const requestBody = JSON.parse(event.body || '{}');
-    const { vehicleName, type, brand, model, mileage, color, energy, dateOfFirstRegistration, dateOfPurchase, numberOfOwner} = requestBody;
+    const {
+      vehicleName,
+      type,
+      brand,
+      model,
+      mileage,
+      color,
+      energy,
+      date_of_first_registration,
+      date_of_purchase,
+      number_of_owner,
+    } = requestBody;
 
     const response = await docClient.send(
       new PutCommand({
-        TableName: 'Vehicles',
+        TableName: 'vehicles-table',
         Item: {
           vehicleName: vehicleName,
           type: type,
@@ -63,23 +82,29 @@ const postVehicles = async (event: any) => {
           mileage: mileage,
           color: color,
           energy: energy,
-          date_of_first_registration: dateOfFirstRegistration,
-          date_of_purchase: dateOfPurchase,
-          number_of_owner: numberOfOwner || null,
+          date_of_first_registration: date_of_first_registration,
+          date_of_purchase: date_of_purchase,
+          number_of_owner: number_of_owner,
         },
-      }),
+      })
     );
 
     return {
       statusCode: 201,
-      body: JSON.stringify({ message: 'Données insérées avec succès.', response }),
+      body: JSON.stringify({
+        message: 'Données insérées avec succès.',
+        response,
+      }),
       headers: corsHeaders,
     };
   } catch (error) {
-    console.error("Erreur lors de l'insertion des données dans DynamoDB:", error);
+    console.error(
+      "Erreur lors de l'insertion des données dans DynamoDB:",
+      error
+    );
     return {
       statusCode: 500,
-      body: JSON.stringify({ message: 'Erreur interne du serveur.' }),
+      body: JSON.stringify({ message: 'INTERNE' + error }),
       headers: corsHeaders,
     };
   }
@@ -89,8 +114,8 @@ const getVehicles = async () => {
   try {
     const response = await docClient.send(
       new ScanCommand({
-        TableName: 'Vehicles',
-      }),
+        TableName: 'vehicles-table',
+      })
     );
 
     return {
@@ -99,16 +124,21 @@ const getVehicles = async () => {
       headers: corsHeaders,
     };
   } catch (error) {
-    console.error('Erreur lors de la récupération des données dans DynamoDB:', error);
+    console.error(
+      'Erreur lors de la récupération des données dans DynamoDB:',
+      error
+    );
     return {
       statusCode: 500,
-      body: JSON.stringify({ message: 'Erreur interne du serveur.' }),
+      body: JSON.stringify({ message: 'INTERNE' + error }),
       headers: corsHeaders,
     };
   }
 };
 
-const putVehicles = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+const putVehicles = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
   try {
     const requestBody: VehicleUpdateRequest = JSON.parse(event.body || '{}');
     const { vehicleName, ...updateFields } = requestBody;
@@ -143,26 +173,32 @@ const putVehicles = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxy
 
     const response = await docClient.send(
       new UpdateCommand({
-        TableName: 'Vehicles',
+        TableName: 'vehicles-table',
         Key: {
           vehicleName: vehicleName,
         },
         UpdateExpression: updateExpression,
         ExpressionAttributeValues: expressionAttributeValues,
         ReturnValues: 'ALL_NEW',
-      }),
+      })
     );
 
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: 'Données mises à jour avec succès.', response }),
+      body: JSON.stringify({
+        message: 'Données mises à jour avec succès.',
+        response,
+      }),
       headers: corsHeaders,
     };
   } catch (error) {
-    console.error('Erreur lors de la mise à jour des données dans DynamoDB:', error);
+    console.error(
+      'Erreur lors de la mise à jour des données dans DynamoDB:',
+      error
+    );
     return {
       statusCode: 500,
-      body: JSON.stringify({ message: 'Erreur interne du serveur.' }),
+      body: JSON.stringify({ message: 'INTERNE' + error }),
       headers: corsHeaders,
     };
   }
@@ -175,23 +211,29 @@ const deleteVehicles = async (event: any) => {
 
     const response = await docClient.send(
       new DeleteCommand({
-        TableName: 'Vehicles',
+        TableName: 'vehicles-table',
         Key: {
           vehicleName: vehicleName,
         },
-      }),
+      })
     );
 
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: 'Données supprimées avec succès.', response }),
+      body: JSON.stringify({
+        message: 'Données supprimées avec succès.',
+        response,
+      }),
       headers: corsHeaders,
     };
   } catch (error) {
-    console.error('Erreur lors de la suppression des données dans DynamoDB:', error);
+    console.error(
+      'Erreur lors de la suppression des données dans DynamoDB:',
+      error
+    );
     return {
       statusCode: 500,
-      body: JSON.stringify({ message: 'Erreur interne du serveur.' }),
+      body: JSON.stringify({ message: 'INTERNE' + error }),
       headers: corsHeaders,
     };
   }
